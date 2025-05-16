fd3bc3c586f462642d1c4b058cb030476b4483df
DongWoonKim <01030985843z@naver.com>
2025-05-16 14:33:05 +0900

    prj setup

---
+++ src/main/java/com/example/spring/clientuiservice/ClientUiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.clientuiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientUiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientUiServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring/clientuiservice/ClientUiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.clientuiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientUiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
